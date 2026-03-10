3ea8d11c8bdb6665639f0a95f6aded6b87daf337
Rajaa <kenbouchr2006@gmail.com>
2026-03-10 15:08:10 +0000

    Correction d'erreurs

--- app/user_manager.py
+++ app/user_manager.py
@@ -1,21 +1,17 @@
-class UserManager :
+class UserManager:
     def __init__(self):
         self.users = []
-
     def add_user(self, username):
         if not username:
             raise ValueError("Le nom d'utilisateur est obligatoire")
         if username in self.users:
             raise ValueError("Utilisateur déja existant")
         self.users.append(username)
-
     def remove_user(self, username):
         if username not in self.users:
             raise ValueError("Utilisateur introuvable")
         self.users.remove(username)
-
     def count_users(self):
         return len(self.users)
-    
     def count_total_users(users):
         return len(users)
